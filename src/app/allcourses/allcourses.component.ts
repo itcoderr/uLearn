ec4786b3e5fbba61da821e21df32207938454ebd
itcoderr <kajpan0709@gmail.com>
2020-07-15 15:58:28 +0530

    after carousel

--- src/app/allcourses/allcourses.component.ts
+++ src/app/allcourses/allcourses.component.ts
@@ -1,3 +1,4 @@
+import { async } from '@angular/core/testing';
 import { ApiService } from './../api.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,13 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllcoursesComponent implements OnInit {
 
-  categories = ['Frontend', 'Banckend', 'Programming', 'Tornado','Bombasto', 'Magneta', 'Tornado'];
   coursecategory : any;
-
+  courseincategory =[];
+  userobj=false;
+  looprun=[];
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.apiService.getAllCategory().subscribe( data => this.coursecategory = data);
+
+    //this.apiService.getAllCategory().subscribe( data => this.coursecategory = data);
+
+    
+    // for (let index = 0; index < this.coursecategory.length; index++) {
+    //   console.log('5');
+    //   var course :any;
+    //   this.apiService.getallcoursesincategory(this.coursecategory[index].categoryID).subscribe( 
+    //   res => course = res);
+    //   this.courseincategory.push(course);
+    // }
+
+    this.getAsyncData();
+
+    
+
+  }
+  async getAsyncData() {
+      this.coursecategory = await this.apiService.getAllCategory();
+      console.log('course category is resolved..');
+      this.getAsyncData1();
+    }
+
+  async getAsyncData1(){
+    for (let index = 0; index < this.coursecategory.length; index++) {
+        var course :any;
+        course = await this.apiService.getallcoursesincategory(this.coursecategory[index].categoryID);
+        this.courseincategory.push(course);
+        if(!((course.length)%4)){
+          this.looprun.push((course.length/4)-1);
+        }
+        else{
+          this.looprun.push( Math.floor(course.length/4));
+        }
+        
+      }
+      console.log(this.courseincategory);
+      console.log(this.looprun);
+      this.userobj=true;
   }
 
 }
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,10 +2,14 @@ import { Customer } from './customer';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const httpOptions = {
+var httpOptions = {
   headers: new HttpHeaders({
-     'Content-Type': 'application/json',
-     'googleID': '01012020' })
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type':'application/json',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Headers' : 'X-Requested-With,content-type',
+    'Access-Control-Allow-Credentials' : 'true'
+  })
 };
 
 @Injectable({
@@ -13,55 +17,33 @@ const httpOptions = {
 })
 export class ApiService {
 
-
-
   constructor(private httpClient: HttpClient) { }
 
   public signin(customer: Customer){
-    httpOptions.headers.append('Access-Control-Allow-Origin', '*');
-    httpOptions.headers.append('Access-Control-Allow-Methods', '*');
-    httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
     return this.httpClient.post('http://localhost:8080/signin',customer, httpOptions);
 }
 
 public signout(customer: Customer){
-  httpOptions.headers.append('Access-Control-Allow-Origin', '*');
-  httpOptions.headers.append('Access-Control-Allow-Methods', '*');
-  httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
   return this.httpClient.put('http://localhost:8080/signout',customer, httpOptions);
 }
 
 public getAllCategory(){
-  httpOptions.headers.append('Access-Control-Allow-Origin', '*');
-  httpOptions.headers.append('Access-Control-Allow-Methods', '*');
-  httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
-return this.httpClient.get('http://localhost:8080/getallcategory', httpOptions);
+return this.httpClient.get('http://localhost:8080/getallcategory', httpOptions).toPromise();
+}
+
+public getallcoursesincategory(categoryid : string){
+  return this.httpClient.get(`http://localhost:8080/getallcoursesincategory?catID=${categoryid}`, httpOptions).toPromise();
 }
 
 public getUser(googleID : string){
-    httpOptions.headers.append('Access-Control-Allow-Origin', '*');
-    httpOptions.headers.append('Access-Control-Allow-Methods', '*');
-    httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
   return this.httpClient.get('http://localhost:8080/get', httpOptions);
 }
 
 public deleteUser(googleID : string){
-    httpOptions.headers.append('Access-Control-Allow-Origin', '*');
-    httpOptions.headers.append('Access-Control-Allow-Methods', '*');
-    httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
   return this.httpClient.delete('http://localhost:8080/delete', httpOptions);
 }
 
 public updateUser(customer: Customer){
-    httpOptions.headers.append('Access-Control-Allow-Origin', '*');
-    httpOptions.headers.append('Access-Control-Allow-Methods', '*');
-    httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
   return this.httpClient.put('http://localhost:8080/update',customer, httpOptions);
 }
 
